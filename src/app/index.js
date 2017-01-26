632949ca036058cffc99df31b0feef886aa706e0
Luca Killmaier <lucakillmaier@lu-co.de>
2017-01-26 22:25:29 +0100

    App-Struktur verschoben

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
-import App from './app/App';
+import App from './main/App';
 import Checkout from './checkout/Checkout';
 
 import Admin from './admin/Admin';
