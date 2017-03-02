a0b39815226770ac53252d601a3b4511bb24850b
Luca Killmaier <lucakillmaier@lu-co.de>
2017-03-02 15:23:31 +0100

    Team Beispieldaten aktualisiert

--- src/app/utils/teams.js
+++ src/app/utils/teams.js
@@ -1,9 +1,9 @@
 module.exports = {
   'FC Steinhofen': [
-    {id:'0', name:'Test Product 1', sizes:['S', 'M', 'L', 'XL'], price:44.95, flockingPrice: 4.50},
-    {id:'1', name:'Test Product 2', sizes:['L', 'XL', 'XXL', 'XXXL'], price:29.50, flockingPrice: 0},
-    {id:'2', name:'Test Product 2', sizes:['L', 'XL', 'XXL', 'XXXL'], price:29.50, flockingPrice: 1},
-    {id:'3', name:'Test Product 2', sizes:['L', 'XL', 'XXL', 'XXXL'], price:29.50},
-    {id:'4', name:'Test Product 2', sizes:['L', 'XL', 'XXL', 'XXXL'], price:29.50}
+    {id:'0', name:'Test Product 1', pricegroups:[{sizes:'128', '134', '152', '164', price: 29.50},{sizes:'S', 'M', 'L', 'XL', price: 34.90}], flockingPrice: 4.50},
+    {id:'1', name:'Test Product 2', pricegroups:[{sizes:'128', '134', '152', '164', price: 29.50},{sizes:'S', 'M', 'L', 'XL', price: 34.90}], flockingPrice: 0},
+    {id:'2', name:'Test Product 2', pricegroups:[{sizes:'128', '134', '152', '164', price: 29.50},{sizes:'S', 'M', 'L', 'XL', price: 34.90}], flockingPrice: 1},
+    {id:'3', name:'Test Product 2', pricegroups:[{sizes:'128', '134', '152', '164', price: 29.50},{sizes:'S', 'M', 'L', 'XL', price: 34.90}], price:29.50},
+    {id:'4', name:'Test Product 2', pricegroups:[{sizes:'128', '134', '152', '164', price: 29.50},{sizes:'S', 'M', 'L', 'XL', price: 34.90}], price:29.50}
   ]
 };
