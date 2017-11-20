dc0903bad7e7a1da0029669f8d0049cae39924b7
lucatk <lucakillmaier@lu-co.de>
2017-11-20 15:58:21 +0100

    SQL-Defaults Update mit Standardwerten

--- defaults.sql
+++ defaults.sql
@@ -3,65 +3,65 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `clubs` (
   `id` int(11) NOT NULL,
-  `name` varchar(256) COLLATE latin1_german1_ci NOT NULL,
+  `name` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
   `logodata` varchar(1024) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
   `displaymode` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci ROW_FORMAT=COMPACT;
 
 CREATE TABLE `customers` (
   `id` int(11) NOT NULL,
-  `clubid` int(11) NOT NULL,
-  `clubname` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `firstname` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `lastname` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `address` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `postcode` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `town` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `email` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `phone` varchar(256) COLLATE latin1_german1_ci NOT NULL
+  `clubid` int(11) NOT NULL DEFAULT '-1',
+  `clubname` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `firstname` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `lastname` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `address` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `postcode` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `town` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `email` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `phone` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
 CREATE TABLE `items` (
   `clubid` int(11) NOT NULL,
   `orderid` int(11) NOT NULL,
   `id` int(11) NOT NULL,
-  `productid` int(11) NOT NULL,
-  `internalid` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `name` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `colour` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
-  `size` varchar(256) COLLATE latin1_german1_ci NOT NULL,
+  `productid` int(11) NOT NULL DEFAULT '-1',
+  `internalid` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `name` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `colour` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '{"id":"-1","name":"","picture":""}',
+  `size` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
   `flockings` varchar(1024) COLLATE latin1_german1_ci NOT NULL DEFAULT '[]',
-  `price` decimal(6,2) NOT NULL,
+  `price` decimal(6,2) NOT NULL DEFAULT '0.00',
   `status` tinyint(4) NOT NULL DEFAULT '-1'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `orders` (
   `clubid` int(11) NOT NULL,
   `id` int(11) NOT NULL,
-  `clubname` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `customerid` int(11) NOT NULL,
+  `clubname` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `customerid` int(11) NOT NULL DEFAULT '-1',
   `status` tinyint(4) NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `products` (
   `clubid` int(11) NOT NULL,
   `id` int(11) NOT NULL,
   `displayorder` int(11) NOT NULL DEFAULT '0',
-  `internalid` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `name` varchar(256) COLLATE latin1_german1_ci NOT NULL,
+  `internalid` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `name` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
   `colours` varchar(1024) COLLATE latin1_german1_ci NOT NULL DEFAULT '[]',
   `pricegroups` varchar(1024) COLLATE latin1_german1_ci NOT NULL DEFAULT '[]',
   `flockings` varchar(1024) COLLATE latin1_german1_ci NOT NULL DEFAULT '[]',
-  `includedFlockingInfo` varchar(256) COLLATE latin1_german1_ci NOT NULL,
+  `includedFlockingInfo` varchar(256) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
   `picture` varchar(1024) COLLATE latin1_german1_ci NOT NULL DEFAULT ''
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `settings` (
   `name` varchar(255) COLLATE latin1_german1_ci NOT NULL,
   `value` varchar(256) COLLATE latin1_german1_ci NOT NULL,
-  `private` tinyint(1) NOT NULL
+  `private` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci ROW_FORMAT=COMPACT;
 
 INSERT INTO `settings` (`name`, `value`, `private`) VALUES
