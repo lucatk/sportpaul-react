c7bdd65e360601091d165e1e13a4458341ad4dde
lucatk <lucakillmaier@lu-co.de>
2018-07-26 19:16:44 +0200

    Kundendaten ausgelagert

--- src/www/php/orders/update.php
+++ src/www/php/orders/update.php
@@ -11,16 +11,9 @@ include('../database.php');
 
 $db = new Database();
 
-$stmt = $db->execute("UPDATE orders SET firstname=:firstName, lastname=:lastName, address=:address, postcode=:postCode, town=:town, email=:email, phone=:phone, status=:status, updated=NOW() WHERE clubid=:clubid AND id=:orderid",
+$stmt = $db->execute("UPDATE orders SET status=:status, updated=NOW() WHERE clubid=:clubid AND id=:orderid",
                       ["clubid" => $_POST["clubid"],
                        "orderid" => $_POST["orderid"],
-                       "firstName" => $_POST["firstName"],
-                       "lastName" => $_POST["lastName"],
-                       "address" => $_POST["address"],
-                       "postCode" => $_POST["postCode"],
-                       "town" => $_POST["town"],
-                       "email" => $_POST["email"],
-                       "phone" => $_POST["phone"],
                        "status" => $_POST["status"]]);
 
 die(json_encode([
